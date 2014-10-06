6029aa3e9fc63a7ddbb488cc5ccd45f6c7d0c2c5
Vik Kamath <vikram.kmth@gmail.com>
2014-10-06 17:28:19 +0300

    incremental changes to lenet.py

--- lenet.py
+++ lenet.py
@@ -170,6 +170,82 @@ def evaluate_lenet5(learning_rate = 0.1,
 
     #Reshape matrix of rasterized images of shape (batch_size,28*28)
     #   into a 4D tensor compatible with our LeNetConvPool Layer
+    layer0_input = x.reshape((batch_size,1,28,28))
+
+    #Construct the first convolutional pooling layer
+    #Filtering reduces the size of the layer to :
+    #       (28-5+1,28-5+1) = (24,24)
+    #Maxpooling reduces this further to :
+    #   (24/2,24/2) = (12,12)
+    #The output, a 4D tensor is therefore of the size:
+    #       (batch_size,nkerns[0],12,12)
+    layer0 = LeNetConvPoolLayer(rng,
+                        input=layer0_input,
+                        image_shape=(batch_size,1,28,28),
+                        filter_shape=(n_kerns[0],1,5,5),
+                        poolsize = (2,2))
+    
+    #The hiddenlayer being fully connected,operates on 2D matrices
+    #of shape:
+    #(batch_size, num_pixels) i.e. a matrix of rasterized images. 
+    #NOTE: I don't understand this
+    #This will generate a matrix of shape (20,32*4*4) = (20,512)
+    #Refer: theano.tensor.flatten
+    layer2_input = layer1.output.flatten(2)
+
+    #Construct a fully connected sigmoidal layer
+    layer2 = HiddenLayer(rng,input=layer2_input,
+                        n_in = n_kerns[1]*4*4,
+                        n_out = 500,
+                        activation = T.tanh)
+
+    #Classify the values of the fully-connected sigmoidal layer
+    layer3 = LogisticRegression(input = layer2.output,
+                                n_in = 500,
+                                n_out = 10)
+
+    #The cost we minimize during training is the 
+    #       Negative Log Likelihood (NLL) of the model
+    cost = layer3.negative_log_likelihood(y)
+
+    #create a function to compute the errors made by the model
+    test_model = theano.function([index],layer3.errors(y),
+                    givens = {
+                        x: test_set_x[index*batch_size:(index+1)*batch_size],
+                        y: test_set_y[index*batch_size:(index+1)*batch_size]})
+
+    validate_model = theano.function([index],layer3.errors(y),
+                            givens = {
+                                x : valid_set_x[index*batch_size:(index+1)*batch_size],
+                                y : valid_set_y[index*batch_size:(index+1)*batch_size]})
+
+    #create a list of all model parameters to be fit by gradient descent
+    params = layer3.params + layer2.params + layer1.params + layer0.params
+
+    #Create a list of gradients for all the model parameters
+    grads = T.grad(cost,params)
+
+    #Train_model is a function that updates the model parameters
+    #   by SGD. 
+    #Since this model has so many parameters, it would be tedious
+    #   to create update rules for each parameter. 
+    #Therefore, we create the updates list by automatically looping
+    #   over all (params[i],grad[i]) pairs. 
+    updates = []
+    for param_i , grad_i in zip(params,grads):
+        updates.append((param_i,param_i - learning_rate*grad_i))
+
+    train_model = theano.function([index],
+                             cost,
+                             updates = updates, 
+                             givens = {
+                                 x : train_set_x[index*batch_size:(index+1)*batch_size],
+                                 y : train_set_x[index*batch_size:(index+1)*batch_size]})
+
+
+                                
+
+
 
 
 
