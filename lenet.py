bbd8e7b7eeda2409056d2814b71ad55dd11086f3
Vik Kamath <vikram.kmth@gmail.com>
2014-10-07 11:31:58 +0300

    incremental changes to lenet.py

--- lenet.py
+++ lenet.py
@@ -243,8 +243,75 @@ def evaluate_lenet5(learning_rate = 0.1,
                                  y : train_set_x[index*batch_size:(index+1)*batch_size]})
 
 
-                                
+    ###############
+    #-TRAIN MODEL-#
+    ###############
 
+    print '......TRAINING.......'
+
+    #early stopping parameters
+    patience = 10000 #look at this many examples no matter what
+    patience_increase = 2 #Wait this much longer when a new best is found
+
+    improvement_threshold = 0.995 #A relative improvement of this much is considered 
+                                #significant
+
+    validation_frequency = min(n_train_batches, patience/2)
+                        #Go through this many 
+                        #minibatches before checking
+                        #the error of the model on the 
+                        #validation set. 
+                        #Here, we check every epoch
+
+    best_params = None
+    best_validation_loss = np.inf
+    best_iter = 0
+    test_score = 0.
+    start_time = time.clock()
+
+    epoch = 0
+    done_looping = False
+
+    while (epoch < n_epochs) and (not done_looping):
+        epoch = epoch + 1
+        for minibatch_index in xrange(n_train_minibatches):
+
+            iter = (epoch-1) * n_train_batches + minibatch_index
+
+            if iter % 100 == 0:
+                print 'training @ iter = ', iter
+            cost_ij = train_model(minibatch_index)
+
+            if (iter+1) % validation_frequency == 0: 
+
+                #Compute zero-one loss on the validation set
+                validation_losses = [validate_model(i) for i
+                                    in xrange(n_valid_batches)]
+                this_validation_loss = np.mean(validation_loss)
+                print('epcoh %i, minibatch %i/%i, validation error %f %%' %\
+                        (epoch,minibatch_index+1,n_train_batches, \
+                            this_validation_loss * 100.))
+
+                #If this is the best validation loss so far
+                if this_validation_loss < best_validation_loss:
+
+                    #Improve patience if loss improvement is good enough
+                    if this_validation_loss < best_validation_loss * \ 
+                                                improvement_threshold
+                            patience = max(patience, iter*patience_increase)
+
+                    #Save the best validaiton score and iteration number
+                    best_validation_loss = this_validation_loss
+                    best_iter = iter
+
+                    #Test it on the test set
+                    test_losses = [test_model(i) for i in xrange(n_test_batches)]
+                    test_score = np.mean(test_losses)
+                    print('epoch %i, minibatch %i\%i, test error of best model %f %%' %\
+                            (
+
+                
+    
 
 
 
